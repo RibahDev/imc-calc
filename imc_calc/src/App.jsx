6a529461f2465090e880010d907d2f4b4ef5fa62
Ribeiro <joao.ribeiro17@outlook.com>
2024-03-23 09:58:01 -0300

    Iniciando Calculadora IMC

---
+++ imc_calc/src/App.jsx
@@ -0,0 +1,16 @@
+import ImcCalc from "./components/ImcCalc";
+
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='container'>
+          <ImcCalc />
+      </div>
+     
+  )
+}
+
+export default App
